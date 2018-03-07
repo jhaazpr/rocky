67a119b4dca1c9ed2d41a11654d07f9bde026b29
Jasper O'Leary <jhaazpr@gmail.com>
2018-03-07 09:08:36 -0800

    Boil down boilerplate

--- Rocky/CreateNetCommand.cs
+++ Rocky/CreateNetCommand.cs
@@ -19,42 +19,17 @@ namespace Rocky
 
         protected override Result RunCommand(Rhino.RhinoDoc doc, RunMode mode)
         {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
-
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint())
+            using (GetObject getObjectAction = new GetObject())
             {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
+                getObjectAction.SetCommandPrompt("Select box for net");
+                if (getObjectAction.Get() != GetResult.Object)
                 {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
+                    RhinoApp.WriteLine("Must select a box.");
+                    return getObjectAction.CommandResult();
                 }
-                pt0 = getPointAction.Point();
-            }
-
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
-            {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DrawLineFromPoint(pt0, true);
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
-            }
-
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
-
-
-            return Result.Success;
+                RhinoApp.WriteLine("Result is {0}",getObjectAction.Object(0));
+                return Result.Success;
+            }   
         }
     }
 }
