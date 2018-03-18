ed60a77ff36cd8a47b7e22861798e27da0404661
Jasper O'Leary <jhaazpr@gmail.com>
2018-03-17 19:49:09 -0700

    messy, but add 4 sides

--- Rocky/CreateNetCommand.cs
+++ Rocky/CreateNetCommand.cs
@@ -22,21 +22,43 @@ namespace Rocky
             const Rhino.DocObjects.ObjectType selFilter = Rhino.DocObjects.ObjectType.PolysrfFilter
                                                         | Rhino.DocObjects.ObjectType.Extrusion;
             Rhino.DocObjects.ObjRef boxObjRef;
-            // Guid boxObjGuid = Guid.Empty;
 
             Result getObjResult = RhinoGet.GetOneObject("Select the box", false, selFilter, out boxObjRef);
             if (getObjResult == Result.Success)
             {
                 Vector3d widthHeightDepthVect = getWidthHeigthDepthVect(boxObjRef);
-                Point3d worldOrigin = new Point3d(0, 0, 0);
-                Rectangle3d rect = MakeRect(worldOrigin, widthHeightDepthVect.X, widthHeightDepthVect.Y);
-                Polyline polyLine = rect.ToPolyline();
+                double xDist = widthHeightDepthVect.X;
+                double yDist = widthHeightDepthVect.Y;
+                double zDist = widthHeightDepthVect.Z;
 
-                if (doc.Objects.AddPolyline(polyLine) != Guid.Empty)
+                // Line 4 rectangles X, Y, X, Y; all Z tall
+
+                Rhino.Collections.RhinoList<Rectangle3d> rectList = new Rhino.Collections.RhinoList<Rectangle3d>();
+
+                Point3d worldOrigin = new Point3d(0, 0, 0);
+                Point3d origin1 = new Point3d(xDist, 0, 0);
+                Point3d origin2 = new Point3d(xDist + yDist, 0, 0);
+                Point3d origin3 = new Point3d(xDist + yDist + xDist, 0, 0);
+
+                Rectangle3d rect0 = MakeRect(worldOrigin, xDist, zDist);
+                Rectangle3d rect1 = MakeRect(origin1, yDist, zDist);
+                Rectangle3d rect2 = MakeRect(origin2, xDist, zDist);
+                Rectangle3d rect3 = MakeRect(origin3, yDist, zDist);
+
+                rectList.Add(rect0);
+                rectList.Add(rect1);
+                rectList.Add(rect2);
+                rectList.Add(rect3);
+
+                Polyline polyline;
+                foreach (Rectangle3d rect in rectList)
                 {
-                    doc.Views.Redraw();
-                    return Result.Success;
+                    polyline = rect.ToPolyline();
+                    doc.Objects.AddPolyline(polyline);
                 }
+                doc.Views.Redraw();
+                return Result.Success;
+
             }
             return Result.Failure;
         }
@@ -56,12 +78,11 @@ namespace Rocky
 
         protected Rectangle3d MakeRect(Point3d origin, double width, double height)
         {
-            Point3d xAxisPt = new Rhino.Geometry.Point3d(width, 0, 0);
-            Point3d yAxisPt = new Rhino.Geometry.Point3d(0, height, 0);
+            Point3d xAxisPt = new Point3d(width, 0, 0) + origin;
+            Point3d yAxisPt = new Point3d(0, height, 0) + origin;
 
-            Point3d worldOrigin = new Point3d(0, 0, 0);
             Vector3d zVector = new Vector3d(0, 0, 1);
-            Plane worldXYPlane = new Rhino.Geometry.Plane(worldOrigin, zVector);
+            Plane worldXYPlane = new Plane(origin, zVector);
 
             Rectangle3d rect = new Rectangle3d(worldXYPlane, xAxisPt, yAxisPt);
             return rect;
