6309885aaefa42e247cd833487120e78073c9090
Jasper O'Leary <jhaazpr@gmail.com>
2018-03-17 18:42:00 -0700

    remove unnecessary namespace thingies

--- Rocky/CreateNetCommand.cs
+++ Rocky/CreateNetCommand.cs
@@ -24,25 +24,28 @@ namespace Rocky
             Rhino.DocObjects.ObjRef boxObjRef;
             // Guid boxObjGuid = Guid.Empty;
 
-            Rhino.Commands.Result getObjResult = Rhino.Input.RhinoGet.GetOneObject("Select the box", false, selFilter, out boxObjRef);
+            Result getObjResult = RhinoGet.GetOneObject("Select the box", false, selFilter, out boxObjRef);
             if (getObjResult == Result.Success)
             {
                 Brep boxBrep = boxObjRef.Brep();
-                Rhino.Geometry.BoundingBox bbox = boxBrep.GetBoundingBox(true);
+                BoundingBox bbox = boxBrep.GetBoundingBox(true);
                 Point3d bboxMin = bbox.Min;
                 Point3d bboxMax = bbox.Max;
                 double xDist = bboxMax.X - bboxMin.X;
                 double yDist = bboxMax.Y - bboxMin.Y;
                 double zDist = bboxMax.Z - bboxMin.Z;
 
-                Point3d origin = new Rhino.Geometry.Point3d(0, 0, 0);
-                Point3d xAxisPt = new Rhino.Geometry.Point3d(xDist, 0, 0);
-                Point3d yAxisPt = new Rhino.Geometry.Point3d(0, yDist, 0);
+                Point3d origin = new Point3d(0, 0, 0);
+                Point3d xAxisPt = new Point3d(xDist, 0, 0);
+                Point3d yAxisPt = new Point3d(0, yDist, 0);
 
-                Plane xyPlane = new Rhino.Geometry.Plane(origin, xAxisPt, yAxisPt);
+                Plane xyPlane = new Plane(origin, xAxisPt, yAxisPt);
 
-                Rhino.Geometry.Rectangle3d rect = new Rhino.Geometry.Rectangle3d(xyPlane, xAxisPt, yAxisPt);
-                Rhino.Geometry.Polyline polyLine = rect.ToPolyline();
+                Rectangle3d rect = new Rectangle3d(xyPlane, xAxisPt, yAxisPt);
+                Polyline polyLine = rect.ToPolyline();
+
+                //Point3d worldOrigin = new Point3d(0, 0, 0);
+                //Rhino.Geometry.Rec
 
                 if (doc.Objects.AddPolyline(polyLine) != Guid.Empty)
                 {
