50668a1970e670e1e16e432ea3df479b5d80bb0c
Jasper O'Leary <jhaazpr@gmail.com>
2018-03-20 00:15:23 -0700

    stub for autoroute

--- Rocky/DefineCircuitComponentsCommand.cs
+++ Rocky/DefineCircuitComponentsCommand.cs
@@ -2,6 +2,7 @@
 using Rhino;
 using Rhino.Commands;
 using Rhino.Geometry;
+using Rhino.Collections;
 using Rhino.Input;
 using Rhino.Input.Custom;
 using Rhino.UI;
@@ -35,9 +36,25 @@ namespace Rocky
             const Rhino.DocObjects.ObjectType selFilter = Rhino.DocObjects.ObjectType.Point;
             Rhino.DocObjects.ObjRef[] pointObjRefs;
 
-            Result getPointsResults = RhinoGet.GetMultipleObjects("Select circuit component endpoints", false, selFilter, out pointObjRefs);
+            Result getPointsResults = RhinoGet.GetMultipleObjects("Select circuit component endpoints",
+                                                                  false, selFilter, out pointObjRefs);
+
+            if (getPointsResults == Result.Success)
+            {
+                RhinoList<Point> circuitPoints = new RhinoList<Point>();
+                foreach (Rhino.DocObjects.ObjRef objRef in pointObjRefs)
+                {
+                    circuitPoints.Add(objRef.Point());
+                    RhinoList<Line> conduitLines = autoroute(circuitPoints);
+                }
+            }
 
             return Result.Success;
+        }
+
+        protected RhinoList<Line> autoroute(RhinoList<Point> points)
+        {
+            return new RhinoList<Point>();
         }
     }
 }
